d538d5b49cc1e8377b241a7a21a56973ddfa4cbd
maintains00 <wltjs2587@gmail.com>
2023-09-09 21:09:59 +0900

    완성

---
+++ demos/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def calculator(request):
+    # return HttpResponse('계산기 기능 구현 시작입니다.')
+    
+    # 1. 데이터 확인
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+    operators = request.GET.get('operators')
+    
+    # 2. 연산
+    if operators == '+':
+        result = int(num1) + int(num2)
+    elif operators == '-':
+        result = int(num1) - int(num2)
+    elif operators == '*':
+        result = int(num1) * int(num2)
+    elif operators == '/':
+        result = int(num1) / int(num2)
+    else:
+        result = 0
+    
+    # 3. 응답
+    return render(request, 'calculator.html', {'result' : result}) # view에서 template를 가져다가 응답
